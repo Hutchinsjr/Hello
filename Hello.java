890002893e32c0a87c450859ba7ccc5c2c141393
Hutchinsjr <46724753+Hutchinsjr@users.noreply.github.com>
2019-01-15 14:05:18 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/***
+*Hello.java
+***
+*Print hello program
+*---
+*Jerry Hutchins
+*01/15/2019
+*CMSC 255 002
+***/
+
+public class Hello {
+	public static void printHeading(){
+		System.out.println("Jerry");
+		System.out.println("Hello.java");
+		System.out.println("Print \"Hello\" program");
+		System.out.println("01/15/2019");
+		System.out.println("CMSC 255 002");
+		System.out.println();
+	}
+
+	public static void main(String [] args){
+		printHeading();
+		System.out.println("Hello");
+	}	
+}	
